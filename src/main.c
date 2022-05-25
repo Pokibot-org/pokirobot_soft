32ef34931b5262a3dfb03c2bb74fc5d95d7e957c
bcluzel <baptiste.clu@gmail.com>
2022-05-26 01:33:49 +0200

    Add end game timeout

--- src/main.c
+++ src/main.c
@@ -1,19 +1,19 @@
 #include <device.h>
 #include <devicetree.h>
 #include <zephyr.h>
-#include "kernel.h"
-#include <drivers/gpio.h>
-#include <logging/log.h>
 
 #include "control/control.h"
 #include "figurine_lifter/figurine_lifter.h"
 #include "hmi/hmi_led.h"
+#include "kernel.h"
 #include "nav/obstacle_manager.h"
 #include "pokarm/pokarm.h"
 #include "shared.h"
 #include "tirette/tirette.h"
 #include "tmc2209/tmc2209.h"
 #include "utils.h"
+#include <drivers/gpio.h>
+#include <logging/log.h>
 
 LOG_MODULE_REGISTER(main);
 
@@ -84,15 +84,26 @@ void match() {
     LOG_DBG("side= %d", side);
     pos2_t dst = {200.0f, 900.0f, 0.25f * M_PI};
     if (side == 0) {
-        dst.x = -dst.x; 
-        dst.a = -dst.a; 
+        dst.x = -dst.x;
+        dst.a = -dst.a;
     }
     control_set_target(&shared_ctrl, dst);
+    uint32_t timeout_counter = 99;
     while (1) {
         LOG_DBG("alive");
         gpio_pin_toggle(led.port, led.pin);
         // control_set_target(&shared_ctrl, dst);
         k_sleep(K_MSEC(1000));
+        timeout_counter--;
+        if (!timeout_counter) {
+            tmc2209_set_speed(&train_motor_1, 0);
+            tmc2209_set_speed(&train_motor_2, 0);
+            tmc2209_set_speed(&train_motor_3, 0);
+            k_sleep(K_MSEC(100));
+            k_sched_lock();
+            while (1) {
+            }
+        }
     }
 exit:
     LOG_INF("MATCH DONE (ret: %d)", ret);
@@ -106,7 +117,7 @@ int main(void) {
 
     // obstacle_manager_init(collision_callback);
 
-    //pokarm_test();
+    // pokarm_test();
     // wait for init
 
     // main thread
