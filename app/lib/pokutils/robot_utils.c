d4944c519c0475b4312ae7587f05d9291da5b899
Baptiste Cluzel <baptiste.clu@gmail.com>
2024-05-03 16:37:25 +0200

    cleanup random

--- app/lib/pokutils/robot_utils.c
+++ app/lib/pokutils/robot_utils.c
@@ -2,11 +2,56 @@
 
 #include <math.h>
 #include <zephyr/kernel.h>
-#include <zephyr/random/random.h>
+
+/*  Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+#include <stdint.h>
+
+/* This is xoshiro128++ 1.0, one of our 32-bit all-purpose, rock-solid
+   generators. It has excellent speed, a state size (128 bits) that is
+   large enough for mild parallelism, and it passes all tests we are aware
+   of.
+
+   For generating just single-precision (i.e., 32-bit) floating-point
+   numbers, xoshiro128+ is even faster.
+
+   The state must be seeded so that it is not everywhere zero. */
+
+
+static inline uint32_t rotl(const uint32_t x, int k) {
+	return (x << k) | (x >> (32 - k));
+}
+
+
+static uint32_t s[4] = {0xDE, 0xAD, 0xBE, 0xEF};
+
+uint32_t next(void) {
+	const uint32_t result = rotl(s[0] + s[3], 7) + s[0];
+
+	const uint32_t t = s[1] << 9;
+
+	s[2] ^= s[0];
+	s[3] ^= s[1];
+	s[1] ^= s[2];
+	s[0] ^= s[3];
+
+	s[2] ^= t;
+
+	s[3] = rotl(s[3], 11);
+
+	return result;
+}
+
 
 inline uint32_t utils_get_rand32(void)
 {
-    return sys_rand32_get();
+    return next();
 }
 
 inline float utils_get_randf(void)
