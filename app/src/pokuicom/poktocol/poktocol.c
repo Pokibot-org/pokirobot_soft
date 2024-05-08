52e4542a039c513a9e1f84dad2ba8a695fbc18b2
bcluzel <baptiste.clu@gmail.com>
2024-05-08 16:11:29 +0200

    potiential bug fix

--- app/src/pokuicom/poktocol/poktocol.c
+++ app/src/pokuicom/poktocol/poktocol.c
@@ -101,6 +101,7 @@ int pokprotocol_feed_byte(struct poktocol *obj, uint8_t byte)
     } else {
         if (obj->receive_index >= MAX_DATA_PAYLOAD_SIZE) {
             obj->receive_index = 0;
+            obj->receiving = false;
             return -1;
         }
         obj->receive_buffer[obj->receive_index] = byte;
